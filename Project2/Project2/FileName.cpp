1a2df4178a67e61372e4d265bc65823690af41c4
agepann111 <KTC24A32A0012@edu.kyoto-tech.ac.jp>
2025-10-11 18:38:03 +0900

    プロジェクト追加

---
+++ Project2/Project2/FileName.cpp
@@ -0,0 +1,16 @@
+#include <Windows.h>
+
+int WINAPI WinMain(
+    HINSTANCE hInstance,      // アプリケーションの識別番号
+    HINSTANCE hPrevInstance,  // 基本使わなくていい
+    LPSTR lpCmdLine,          // コマンドライン引数（起動時のオプション）
+    int nCmdShow              // ウィンドウの表示方法（最大化、最小化など）
+)
+{
+    // ここにメインの処理を書く
+    // 1. ウィンドウクラス登録
+
+    // 2. ウィンドウ作成
+    // 3. メッセージループ
+    return 0;
+}
